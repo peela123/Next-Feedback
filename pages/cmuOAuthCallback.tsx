72e4b9ae18faadd15cba85fdbd9b20ec6dcd431b
Peera Aronrat <peelaxv@gmail.com>
2024-03-29 18:30:25 +0700

    sdsd

--- pages/cmuOAuthCallback.tsx
+++ pages/cmuOAuthCallback.tsx
@@ -35,7 +35,7 @@ export default function CMUOAuthCallback() {
           setMessage("Unknown error occurred. Please try again later.");
         }
       });
-  }, [code, router]);
+  }, [code]);
 
   // return <div className="p-3">{message || "Redirecting ..."}</div>;
   return <div className="p-3">{message || "Authenticating ..."}</div>;
